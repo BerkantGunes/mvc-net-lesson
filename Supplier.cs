a699ef45a51653f0eefb6fc3fe0f22199476bdad
Berkant Gunes <gunesberkant96@gmail.com>
2025-06-20 15:02:35 +0300

    Add files via upload

---
+++ Supplier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Data.Model
+{
+    public class Supplier
+    {
+        public int SupplierId { get; set; }
+        public string Name { get; set; }
+        public string ContactInfo { get; set; }
+    }
+}
