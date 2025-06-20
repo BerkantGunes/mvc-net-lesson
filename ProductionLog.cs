12649682674900bc2746118dd3f9ea4bd35f29c1
Berkant Gunes <gunesberkant96@gmail.com>
2025-06-20 15:01:58 +0300

    Add files via upload

---
+++ ProductionLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Data.Model
+{
+    public class ProductionLog
+    {
+        public int ProductionLogId { get; set; }
+        public int ProductionOrderId { get; set; }
+        public int MachineId { get; set; }
+        public int ProducedQuantity { get; set; }
+        public DateTime Timestamp { get; set; }
+        public ProductionOrder ProductionOrder { get; set; }
+        public Machine Machine { get; set; }
+    }
+}
